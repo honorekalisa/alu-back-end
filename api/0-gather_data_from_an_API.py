1d37301a8df26ca57563025a51a8143db2710f52
honorekalisa <h.kalisa@alustudent.com>
2023-07-09 23:34:24 +0200

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,53 +1,29 @@
 #!/usr/bin/python3
-"""Displays tasks information of a given employee
-Trying two lines
-"""
-from requests import get
-from sys import argv
+"""Module"""
+
+import requests
+import sys
 
 
-def get_data(url):
-    """Utility method to get data from any api endpoint and parse the json
-    params:
-        - url (str) - the URL endpoint
-    """
-    request = get(url)
+"""Module"""
 
-    if request.get('status_code') == 200:
-        try:
-            return request.json()
-        except:
-            raise Exception("")
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(user_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(user_id)
 
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
 
-def main():
-    """The main function from which the whole program starts
-    """
-    # Extract the user id from the script arguments
-    employee_id = argv[1]
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
 
-    # Getting the name
-    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    name = get_data(user_url).get('name')
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
 
-    # Getting the todos
-    tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    todos = get_data(tasks_url)
-
-    total_number_of_todos = len(todos)
-    number_of_completed_todos = 0
-
-    for todo in todos:
-        if todo.get('completed'):
-            number_of_completed_todos += 1
-
-    print(
-        f'Employee {name} is done with tasks({number_of_completed_todos}/{total_number_of_todos})')
-
-    for todo in todos:
-        if todo.get('completed'):
-            print(f'\t {todo.get("title")}')
-
-
-if __name__ == "__main__":
-    main()
+    [print("\t " + task["title"]) for task in task_completed]
